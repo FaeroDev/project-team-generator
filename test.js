c515f304e1d621808d51f73c24301686d8bf4396
Matthew Pawlak <pharaohnof@gmail.com>
2021-05-10 20:49:54 -0400

    currently working in test.js....started inquirer prompts for adding members with switch statements, will store team members in array...next few updates will be incrementl to this one

---
+++ test.js
@@ -0,0 +1,153 @@
+console.log("teamgen fired");
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+
+const Manager = require("./lib/ManagerClass");
+
+const fillHTML = require("./src/fillHTML.js");
+const styleFill = require("./src/styleFill.js");
+
+const fsMkdir = util.promisify(fs.mkdir);
+const writeFile = util.promisify(fs.writeFile);
+
+let teamArray = [];
+let userAnswers;
+
+const managerInput = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter Team Manager Name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter Team Manager ID number:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter Team Manager Email Address:",
+      },
+      {
+        type: "input",
+        name: "office",
+        message: "Enter Team Manager Office Number:",
+      },
+    ])
+
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.office
+      );
+      console.log(manager);
+      teamArray.push(manager);
+      console.log(teamArray);
+      addMemberQuery();
+    });
+};
+
+const addMemberQuery = () => {
+  return inquirer
+    .prompt({
+      type: "list",
+      name: "addTeamMember",
+      message: "Add an Engineer or Intern?",
+      choices: ["Yes", "No"],
+    })
+
+    .then((answer) => {
+      switch (answer.addTeamMember) {
+        case "Yes":
+          selectInput();
+          break;
+        case "No":
+          console.log("answered no to addTeamMember");
+          break;
+      }
+    })
+    .catch((err) => console.error(err));
+};
+
+const selectInput = () => {
+  return inquirer
+    .prompt({
+      type: "list",
+      name: "selectRole",
+      message: "Select member to add (or abort adding a member):",
+      choices: ["Engineer", "Intern", "ABORT"],
+    })
+    .then((answer) => {
+      switch (answer.selectRole) {
+        case "Engineer":
+          console.log("CHOSE ENGINEER");
+          // engineerInput();
+          break;
+
+        case 'Intern':
+          console.log("CHOSE INTERN");
+          // internInput();
+          break;
+
+        case "ABORT":
+          addMemberQuery()
+          break;
+      }
+    });
+};
+
+managerInput();
+
+// // const init = () => {
+// // .then((answers) => writeFile('test.html', fillHTML(answers)))
+
+// userInput()
+//   // .then(() => {
+//   //   fsMkdir("./dist", { recursive: true }, (err) =>
+//   //     err
+//   //       ? console.log(err)
+//   //       : console.log("Successfully created dist directory")
+//   //   );
+//   // })
+//   .then((answers) => {
+//     //   fsMkdir("./dist", { recursive: true }, (err) =>
+//     //   err
+//     //     ? console.log(err)
+//     //     : console.log("Successfully created dist directory")
+//     // );
+
+//     fsMkdir("./dist", { recursive: true });
+//     console.log("Successfully created dist directory");
+
+//     writeFile("./dist/team.html", fillHTML(answers));
+//     this.Answers = answers;
+//     userAnswers = this.Answers;
+//     console.log(userAnswers);
+
+//     console.log(`
+//            YOU ENTERED:
+//     `);
+//     console.log(answers);
+//     console.log(`
+//       ----------------------------------------------------------------
+//       GENERATED:
+//        ./dist/team-style.css, and
+//        ./dist/team.html
+//        IN CURRENT DIRECTORY`);
+//   })
+
+//   .then(() => {
+//     writeFile("./dist/team-style.css", styleFill);
+//   })
+
+//   .catch((err) => console.error(err));
+// };
+
+// init();
